4e7f5c75209aa45d94d46bf1e919df89f80723e3
Travis Welch <trav.cwelch@gmail.com>
2020-10-21 19:51:36 -0700

    update scraper

--- app/scraper.py
+++ app/scraper.py
@@ -18,11 +18,6 @@ class Scraper():
 
         for row in vocab_list:
 
-            kanji = row.find('div', class_='f_kanji')
-            extra = row.find('div', class_='jukugo')
-            furigana = row.find('div', class_='furigana')
-            reading  = row.find('div', class_='vm')
-
             #first = reading.text.find('1.')
             #second = reading.text.find('2.')
 
@@ -31,23 +26,23 @@ class Scraper():
             #definition_two = reading.text[second:]
 
             try:
+                kanji = row.find('div', class_='f_kanji').text
+                extra = row.find('div', class_='jukugo').text.replace('\n', '')
+                reading  = row.find('div', class_='vm').text
+                furigana = row.find('div', class_='furigana').text
+
+                word = kanji + extra.replace(furigana + kanji, '').replace('\n', '')
                 level = 'N' + row.find('div', class_='w_ref').text
-                #first = reading.text.find('1.')
-                pos = reading.find('span').text
-                definition = reading.text.replace(pos, '').replace('1. ', '')
+                pos = row.find('div', class_='vm').find('span').text
+                definition = reading.replace(pos, '').replace('1. ', '')
 
                 usefulness = row.find('div', class_='ufn_container').text.replace('\n', '')
             except:
                 continue
 
             print('THE JLPT LEVEL OF THIS WORD IS ' + level)
-
-            try:
-                print(furigana.text)
-                print((kanji.text + extra.text.replace(furigana.text + kanji.text, '')).replace('\n', ''))
-            except:
-                print(extra.text.replace('\n', ''))
-
+            print('FURIGANA: ' + furigana)
+            print('WORD: ' + word)
             print('PART OF SPEACH: ' + pos)
             print('DEFINITION: ' + definition)
             print('USEFULNESS: ' + usefulness + '\n')
@@ -63,9 +58,7 @@ class Scraper():
             #print(definition_two)
 
     def iterate_endpoints(self):
-        #self.BASE_URL = "https://www.kanshudo.com/collections/wikipedia_jlpt/"
         self.set_endpoints()
-        #print(self.ENDPOINTS)
 
         for endpoint in self.ENDPOINTS:
 
@@ -77,8 +70,6 @@ class Scraper():
 
 def main():
     sc = Scraper()
-    #sc.get_endpoints()
-    #print(sc.ENDPOINTS)
     sc.iterate_endpoints()
 
 if __name__ == '__main__':
